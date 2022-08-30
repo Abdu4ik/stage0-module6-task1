6b0acc9a9ce3e2837f525e38fbb566988e249e0d
Abdu4ik <107094143+Abdu4ik@users.noreply.github.com>
2022-08-30 23:57:23 +0500

    Update Dog.java

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,8 +1,11 @@
 package com.epam.mjc.stage0;
 
 public class Dog extends Animal{
-
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
     public Dog() {
         super("brown", 4, true);
     }
+
 }
